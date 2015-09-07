f49b8b1488c4fd2689168ea7c902269be14e1dcb
Justin Sippel essdot <justinsippel+gh@gmail.com>
2015-09-07 14:43:28 -0700

    Updates

--- src/index.js
+++ src/index.js
@@ -1,39 +1,37 @@
 module.exports = testNarcNumber
 module.exports.searchGenerator = searchGenerator
 
-function testNarcNumber (n) {
-  n = Math.abs(Number(n))
-
+function testNarcNumber (_n) {
+  const n = sanitize(_n)
   const digits = getDigits(n)
-  const sum = digits.reduce((prev, current) => {
-    return prev + Math.pow(current, 3)
-  }, 0)
+  const digitsToPower = digits.map(digit => Math.pow(digit, digits.length))
+  const digitsSum = digitsToPower.reduce((a, b) => a + b, 0)
 
-  if (!isFinite(n) || !isFinite(sum)) {
-    return false
+  return isFinite(n) && isFinite(digitsSum) && digitsSum === n
+}
+
+function * searchGenerator (arg) {
+  let i = 0
+
+  while (true) {
+    let startAt = null
+
+    if (testNarcNumber(i)) {
+      startAt = yield i
+    }
+
+    if (typeof startAt === 'number') {
+      i = sanitize(startAt)
+    } else {
+      i++
+    }
   }
-
-  return sum === n
 }
 
 function getDigits (n) {
   return n.toFixed(0).split('').map(Number)
 }
 
-function * searchGenerator (arg) {
-  let i = 2
-  let startAt
-
-  while (true) {
-    if (testNarcNumber(i)) {
-      startAt = yield i
-    }
-
-    if (typeof startAt === 'number') {
-      i = startAt
-      startAt = null
-    } else {
-      i++
-    }
-  }
+function sanitize (n) {
+  return Math.abs(Number(n))
 }
--- test/index.js
+++ test/index.js
@@ -9,7 +9,6 @@ test('matches sum', t => {
   t.ok(testNarcNumber(0))
   t.ok(testNarcNumber(1))
 
-  t.notOk(testNarcNumber(2))
   t.notOk(testNarcNumber(100))
   t.notOk(testNarcNumber(152))
   t.notOk(testNarcNumber(154))
@@ -21,10 +20,10 @@ test('generator', t => {
   const gen = testNarcNumber.searchGenerator()
   const gen2 = testNarcNumber.searchGenerator()
 
-  t.equal(gen.next().value, 153)
-  t.equal(gen2.next().value, 153)
-  t.equal(gen.next().value, 370)
-  t.equal(gen2.next().value, 370)
+  t.equal(gen.next().value, 0)
+  t.equal(gen2.next().value, 0)
+  t.equal(gen.next().value, 1)
+  t.equal(gen2.next().value, 1)
 
   t.end()
 })
@@ -32,11 +31,10 @@ test('generator', t => {
 test('generator takes argument', t => {
   const gen = testNarcNumber.searchGenerator()
 
-  t.equal(gen.next().value, 153)
-  t.equal(gen.next(2).value, 153)
-  t.equal(gen.next(3).value, 153)
-  t.equal(gen.next().value, 370)
-  t.equal(gen.next().value, 371)
+  t.equal(gen.next().value, 0)
+  t.equal(gen.next(2).value, 2)
+  t.equal(gen.next(3).value, 3)
+  t.equal(gen.next(150).value, 153)
   t.equal(gen.next(370).value, 370)
   t.equal(gen.next(371).value, 371)
   t.equal(gen.next().value, 407)
